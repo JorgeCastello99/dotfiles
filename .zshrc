7729ea5b8be0e6e51934994d204d3112288317a1
Jorge Castelló <jcaste999@gmail.com>
2022-09-21 15:36:40 +0200

    actualizacion

--- .zshrc
+++ .zshrc
@@ -1,1 +1,3 @@
 source ~/.dotfiles/.aliases
+
+echo "$(cat $HOME/.dotfiles/banner1"
--- install.sh
+++ install.sh
@@ -5,17 +5,24 @@ sudo apt update && sudo apt upgrade
 sudo apt autoremove
 
 #instalaciones
-sudo apt install bash zsh zgen sudo wget git curl exa
+sudo apt install bash dpkg zsh zgen sudo wget git curl exa tldr lolcat
 
 #bat
 wget https://github.com/sharkdp/bat/releases/download/v0.22.1/bat-musl_0.22.1_amd64.deb
 sudo apt install ./bat-musl_0.22.1_i686.deb
 
 #exa
-EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+if [dpkg -s exa]
+then
+    
+else
+    EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip" 
 sudo unzip -q ./exa.zip bin/exa -d /usr/local
 rm -rf exa.zip
+
+fi
+
 
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
