cda28ede19f0064fc46671bdeaf2c5ddc6f9056a
Jorge Castelló <jcaste999@gmail.com>
2022-09-22 18:18:20 +0200

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -16,12 +16,26 @@ if [dpkg -s exa]
 then
     
 else
+
     EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip" 
 sudo unzip -q ./exa.zip bin/exa -d /usr/local
 rm -rf exa.zip
 
 fi
+
+
+#node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+
+#pnpm mas rapido mas rapido que npm reutiliza de otros proyectos los paquetes
+sudo npm install -g pnpm
+
+#mkweb
+sudo curl -s https://manz.dev/download/mkweb -o /usr/local/bin/mkweb
+sudo chmod +x /usr/local/bin/mkweb
+
 
 
 # Oh-my-zsh
